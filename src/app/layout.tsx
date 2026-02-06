b38713e9d27702f635d309bfdf8489ff1a9a34e7
NotHarshhaa <reddyharshhaa12@gmail.com>
2026-02-06 11:11:18 +0530

    Kubernetes-dashboard: update font settings in global styles and layout
    - Replaced the Geist font with Inter for both sans and mono font styles in `globals.css` and `layout.tsx`.
    - Adjusted the font variable usage in the layout to reflect the new font settings, ensuring a consistent typography across the application.
    
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -37,9 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} font-sans antialiased`}>
         {children}
       </body>
     </html>
